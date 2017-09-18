7d2a90ae6ce05e3af381a83072fbafa5f0d79d89
= <=>
2017-09-18 09:22:09 -0400

    Added a factory.

---
+++ tcp-client-wrapper/ITcpClientWrapperFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace tcp_client_wrapper
+{
+    public interface ITcpClientWrapperFactory
+    {
+        ITcpClientWrapper Create();
+        ITcpClientWrapper Create(TcpClient tcpClient);
+        ITcpClientWrapper Create(IPEndPoint localEP);
+        ITcpClientWrapper Create(AddressFamily family);
+        ITcpClientWrapper Create(string hostname, int port);
+    }
+}
---
+++ tcp-client-wrapper/TcpClientWrapperFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace tcp_client_wrapper
+{
+    public class TcpClientWrapperFactory : ITcpClientWrapperFactory
+    {
+        public ITcpClientWrapper Create()
+        {
+            return new TcpClientWrapper();
+        }
+
+        public ITcpClientWrapper Create(TcpClient tcpClient)
+        {
+            return new TcpClientWrapper(tcpClient);
+        }
+
+        public ITcpClientWrapper Create(IPEndPoint localEP)
+        {
+            return new TcpClientWrapper(localEP);
+        }
+
+        public ITcpClientWrapper Create(AddressFamily family)
+        {
+            return new TcpClientWrapper(family);
+        }
+
+        public ITcpClientWrapper Create(string hostname, int port)
+        {
+            return new TcpClientWrapper(hostname, port);
+        }
+    }
+}
